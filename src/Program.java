f8d47d4d3baab55759acf0951d6bd4acc63cb24a
Arthur Alves <arthur_s.a@hotmail.com.br>
2019-04-14 00:47:07 -0300

    Add a Welcome message

--- src/Program.java
+++ src/Program.java
@@ -10,4 +10,11 @@
  */
 public class Program {
     
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Olá mundo");
+        System.out.println(" Teste GitHub");
+    }
+    
 }
